3a16118918be3f2857682e3092fcc7096194a72d
itzsst-gif <sivasridhar80@gmail.com>
2026-03-26 14:51:05 +0530

    UC2

--- src/main/java/com/step/App.java
+++ src/main/java/com/step/App.java
@@ -5,7 +5,7 @@ public class App
     public static void main(String[] args)
     {
         if (args.length == 0) {
-            System.out.println("Hello, Stranger!");
+            System.out.println("Hello, World!");
         } else {
             String name = args[0];
             System.out.println("Hello, " + name + "!");
