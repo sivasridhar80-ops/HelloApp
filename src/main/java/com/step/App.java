1623478e405089123e4261ce7eaaee617767d4fd
sivasridhar80-ops <sivasridhar80@gmail.com>
2026-04-21 15:07:48 +0530

    UC3

--- src/main/java/com/step/App.java
+++ src/main/java/com/step/App.java
@@ -1,14 +1,15 @@
 package com.step;
 
-public class App
-{
-    public static void main(String[] args)
-    {
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
+public class App{
+
+    public static void main(String[] args) {
+
+        String name = "World";   // default value
+
+        if (args.length > 0) {
+            name = args[0];
         }
+
+        System.out.println("Hello " + name);
     }
 }
