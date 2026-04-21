2ad26ad47fc9fc1c562d14fceb3f531788fc358f
sivasridhar80-ops <sivasridhar80@gmail.com>
2026-04-21 15:12:54 +0530

    UC4

--- src/main/java/com/step/App.java
+++ src/main/java/com/step/App.java
@@ -1,15 +1,17 @@
 package com.step;
 
-public class App{
+public class App {
 
     public static void main(String[] args) {
 
-        String name = "World";   // default value
-
-        if (args.length > 0) {
-            name = args[0];
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        }
+        else {
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
         }
 
-        System.out.println("Hello " + name);
     }
 }
